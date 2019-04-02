8695073cc5f13537578c04486021db580daacbc8
João Pedro <joaopdante@gmail.com>
2019-04-02 03:39:46 -0300

    Remove Arquivos

--- Content/Scripts/test.js
+++
@@ -1,8 +0,0 @@
-const uclass = require('uclass')().bind(this,global)
-class MySMA extends StaticMeshActor {
-  ctor() {
-    this.StaticMeshComponent.SetStaticMesh(StaticMesh.Load('/Engine/BasicShapes/Cube.Cube'))
-  }
-}      
-let MySMA_C = uclass(MySMA) 
-new MySMA_C(GWorld,{Z:200})
